1b64b09a6e13646092c20767d4167fcc7f328987
Tefery <Tefery@Ultimate-Gaming>
2017-02-19 02:05:20 +0100

    a

---
+++ ordenExcepciones.java
@@ -0,0 +1,12 @@
+// Servidor
+
+BindException // Si el puerto ya está en uso
+SocketException // Si el cliente cierra mal
+IOException // General
+
+// Cliente
+
+ConnectException // Si el host no está escuchando por el puerto indicado
+UnknownHostException // Si no encuentra el host
+SocketException // Si el servidor cierra mal
+IOException // General
